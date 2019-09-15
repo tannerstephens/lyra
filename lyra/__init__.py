ed09945affea4eed8cc12acaf3cc854f040d7093
Tanner Stephens <tannerrstephens@gmail.com>
2019-09-15 18:31:32 -0500

    Added groups resources

--- lyra/__init__.py
+++ lyra/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 def create_app(config='lyra.config.Config'):
   app = Flask(__name__)
+  CORS(app)
 
   app.config.from_object(config)
 
   with app.app_context():
     from lyra.api import register_api
     register_api(app)
-
-    from lyra.views import views
-    app.register_blueprint(views)
 
     from lyra.models import db
     migrate = Migrate(app, db)
--- lyra/api/groupme_api.py
+++ lyra/api/groupme_api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, session, request
 from flask_restful import Api, Resource
 from lyra.lib.groupme import GroupmeApi
+from lyra.models import Group, db
 from .utils import success_message
 from .error_messages import *
 
@@ -29,6 +30,44 @@ class Groups(Resource):
   def get(self):
     access_token = session.get('access_token')
 
+    if access_token:
+      gapi = GroupmeApi(access_token)
+
+      page = request.args.get('page', 1)
+
+      groups = gapi.get_groups(page)
+
+      return success_message(True, groups=groups)
+
+    
+    return success_message(False, error=NOT_LOGGED_IN)
 
 
-api.add_resource(CurrentUser, '/current_user')
+class Group(Resource):
+  def get(self, group_id):
+    access_token = session.get('access_token')
+
+    if access_token:
+      gapi = GroupmeApi(access_token)
+
+      group = gapi.get_group(group_id)
+
+      if group:
+        groupme_id = group.get('id')
+        db_group = Group.query.filter_by(group_id=group_id).first()
+
+        if db_group:
+          managed = True
+        else:
+          managed = False
+
+        return success_message(True, group=group, managed=managed)
+      else:
+        return success_message(False, error=GROUPME_NOT_AUTHORIZED)
+    
+    return success_message(False, error=NOT_LOGGED_IN)
+
+
+api.add_resource(CurrentUser, '/current-user')
+api.add_resource(Groups, '/groups')
+api.add_resource(Groups, '/groups/<int:group_id>')
