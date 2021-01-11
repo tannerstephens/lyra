dfea01e97c59a2ce6eca81c9a7eb5610cdf06348
Tanner Stephens <tannerrstephens@gmail.com>
2021-01-10 21:09:31 -0600

    Simon Says

--- lyra/lyra_bot/plugins/like_mentions.py
+++ lyra/lyra_bot/plugins/like_mentions.py
@@ -4,12 +4,12 @@ def handle(data, groupme_api):
   if 'attachments' not in data:
     return
 
-  mentions = list(filter(lambda attachment: attachment['type'] == 'mentions', data['attachments']))
+  mentions = list(filter(lambda attachment: attachment['type'] == 'mentions', data['attachments']))[0]
 
   if len(mentions) == 0:
     return
 
-  if groupme_api.me_data['user_id'] not in mentions[0]['user_ids']:
+  if groupme_api.me_data['user_id'] not in mentions['user_ids']:
     return
 
   groupme_api.like_message(data['group_id'], data['id'])
---
+++ lyra/lyra_bot/plugins/simon_says.py
@@ -0,0 +1,33 @@
+NAME = 'Simon Says'
+
+def handle(data, groupme_api):
+  if '!say' == data['text'].lower()[:4]:
+    message = data['text'][4:]
+
+    mentions = list(filter(lambda attachment: attachment['type'] == 'mentions', data['attachments']))[0]
+
+    pre_strip_length = len(message)
+
+    message = message.strip()
+
+    delta = 4 + (pre_strip_length - len(message))
+
+    locations = mentions['loci']
+
+    new_locations = []
+
+    for location in locations:
+      new_locations.append([
+        location[0] + delta,
+        location[1] + delta
+      ])
+
+    attachments = [
+      {
+        'loci': new_locations,
+        'user_ids': mentions['user_ids'],
+        'type': 'mentions'
+      }
+    ]
+
+    groupme_api.send_message(data['group_id'], message, attachments)
